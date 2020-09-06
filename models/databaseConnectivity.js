971a6d5bc434bdba697d0b3f4fa1035f4d320be6
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2020-09-06 16:41:31 +0530

    Delete databaseConnectivity.js

--- models/databaseConnectivity.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI,{ useUnifiedTopology:true ,useNewUrlParser:true},(err) =>{
-    if(!err){console.log('connected to the database')}
-    else{console.log('error in connection:'+JSON.stringify (err,undefined,2))}
-});
-
-require('./user');
-require('./pitcher');
